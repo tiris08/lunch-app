6eb51afc6d4dd10c60bda9d782d4cb3274b6d7f9
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-09-27 17:15:52 +0300

    configure shoulda matcher

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -68,4 +68,12 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 
   config.include Helpers::DailyMenu, type: :controller
+
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
