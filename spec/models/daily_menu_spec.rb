6e3902444e2c39db56e67ea13370a3ceb69ed1f5
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-09-27 17:17:17 +0300

    add daily_menu model tests

---
+++ spec/models/daily_menu_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe DailyMenu, type: :model do
+  
+  it { should accept_nested_attributes_for(:food_items).allow_destroy(true) }
+  it { should have_many(:food_items) }
+  it { should have_many(:orders) }
+end
