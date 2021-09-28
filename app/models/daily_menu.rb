0311982cc81f0315258e7d64d40d7c2b56b4d2a1
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-09-28 16:56:22 +0300

    delete unnecessary arguments for daily_menu association

--- app/models/daily_menu.rb
+++ app/models/daily_menu.rb
@@ -1,5 +1,5 @@
 class DailyMenu < ApplicationRecord
-  has_many :food_items, inverse_of: :daily_menu, dependent: :destroy
+  has_many :food_items
   has_many :orders
   accepts_nested_attributes_for :food_items, allow_destroy: true, reject_if: :all_blank
 end
