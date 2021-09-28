a1fa1c093253f185952ef710f4d0857f1b399e5e
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-09-28 16:55:16 +0300

    add limit 3 for order orderitems attributes

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy, inverse_of: :order
-  accepts_nested_attributes_for :order_items
+  accepts_nested_attributes_for :order_items, limit: 3
   has_many :food_items, through: :order_items
   belongs_to :user
   belongs_to :daily_menu
