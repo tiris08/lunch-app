2f1e0d8064cd6f1a5e148bde25e0356acd3964a4
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-09-28 16:36:59 +0300

    add signing up feature spec

--- spec/features/dashboard_page_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-feature 'dashboard page' do
-  # scenario 'redirect to Log-in' do
-  #   visit('/')
-  #   expect(page).to have_content('Log-in') 
-  # end
-end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,5 @@
-require 'simplecov'
-SimpleCov.start
+# require 'simplecov'
+# SimpleCov.start
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require 'support/database_cleaner'
@@ -8,6 +8,7 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'capybara/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
