53b4f68b3ba5240ec470f6c00b05b622099f1bde
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-09-28 10:49:21 +0300

    add and configure simplecov gem for seeing tests coverege

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,5 @@
+require 'simplecov'
+SimpleCov.start
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require 'support/database_cleaner'
@@ -31,6 +33,7 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
