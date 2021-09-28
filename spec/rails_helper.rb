2fa526ba7b44cc6b352a0c17efda12a30343a7aa
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-09-28 10:13:37 +0300

    organize spec support directory

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,17 +63,4 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  config.include Devise::Test::ControllerHelpers, type: :controller
-
-  config.include FactoryBot::Syntax::Methods
-
-  config.include Helpers::DailyMenu, type: :controller
-
-end
-
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
 end
