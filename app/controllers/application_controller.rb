97b705d7b3c7f11de92b5ad4825cbd92f22dab95
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-09-29 10:07:36 +0300

    customize devise's after_sign_in_path and after sign_out_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    current_user.is_admin? ? admin_root_path : root_path
+  end
+
+  def after_sign_out_path_for(scope)
+    new_user_session_path
+  end
 end
