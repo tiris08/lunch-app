2992ee262589175459380eaf1e85428b7877b527
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-09-29 10:05:23 +0300

    move out devise strong params methods from application_controller to registrations_controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,14 @@ class RegistrationsController < Devise::RegistrationsController
 
   protected
 
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+  end
+
   def after_sign_up_path_for(resource)
     current_user.is_admin? ? admin_root_path : root_path
   end
