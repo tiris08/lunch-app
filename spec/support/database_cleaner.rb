1d030e093e0ed32a985babd9d25c3c2ef665cc86
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-10-07 17:28:23 +0300

    Update spec support config for feature tests includng js

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,17 +1,23 @@
 RSpec.configure do |config|
 
   config.before(:suite) do
-    DatabaseCleaner.clean_with :truncation, except: %w(ar_internal_metadata)
+    DatabaseCleaner.clean_with(:truncation)
   end
+  
   config.before(:each) do
+    # set the default
     DatabaseCleaner.strategy = :transaction
   end
-
+  
+  config.before(:each, type: :feature) do
+    DatabaseCleaner.strategy = :truncation
+  end
+  
   config.before(:each) do
     DatabaseCleaner.start
   end
-
-  config.after(:each) do
+  
+  config.append_after(:each) do
     DatabaseCleaner.clean
   end
 end
