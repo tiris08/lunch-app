be95d447afded8248be2f5bae084b2dd7291dfd4
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-10-13 17:06:50 +0300

    remove waste js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,24 +25,8 @@ $(document).on('turbolinks:load', function() {
   
   $('select.dropdown')
   .dropdown()
-
-  // order update form
-
-  $('.ui.three.column.grid').on('cocoon:after-insert', function() {
-    check_to_hide_or_show_add_link();
-  });
-  $('.ui.three.column.grid').on('cocoon:after-remove', function() {
-    check_to_hide_or_show_add_link();
-  });
-  check_to_hide_or_show_add_link();
-  function check_to_hide_or_show_add_link() {
-    if ($('.nested-fields.column:visible').length == 3) {
-      $('.ui.primary.labaled.button.nest').hide();
-    } else {
-      $('.ui.primary.labaled.button.nest').show();
-    }
-  }
 })
+
 
 // if you wan't to use custom variables, you should import custom styelesheet
 // import 'stylesheets/semantic-ui.scss'
