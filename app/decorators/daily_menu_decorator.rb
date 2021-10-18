9d71ba43a07c973b24b8c467a0808a4ee8bbc251
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-10-18 14:23:45 +0300

    delete already existing in facade method

--- app/decorators/daily_menu_decorator.rb
+++ app/decorators/daily_menu_decorator.rb
@@ -12,12 +12,4 @@ class DailyMenuDecorator < ApplicationDecorator
   def created_at_day 
     object.created_at.strftime("%A")
   end
-
-  def edit_or_create_menu_link
-    if object.created_at.today?
-      h.link_to "Edit a menu for today ", h.edit_admin_daily_menu_path(object), class: "ui primary large button"
-    else
-      h.link_to "Create a menu for today", h.new_admin_daily_menu_path, class: "ui primary large button" 
-    end
-  end
 end
