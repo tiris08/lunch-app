b7f40bfc9d9d97a471731436c444c8bb0713ba28
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-10-19 09:34:40 +0300

    move decorate method in facade

--- app/facades/daily_menus/show_facade.rb
+++ app/facades/daily_menus/show_facade.rb
@@ -14,5 +14,9 @@ module DailyMenus
     def user_order_cost
       @user_order_cost ||= user_order&.food_items&.pluck(:price)&.sum
     end
+
+    def user_order_items
+      @user_order_items ||= user_order&.food_items.decorate
+    end
   end
 end
