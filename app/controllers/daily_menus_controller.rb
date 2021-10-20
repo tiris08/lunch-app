f37fbcda8b3d28cd6af0a8e94827535f03d89237
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-10-20 17:38:33 +0300

    move user order logic from daily_menus show facade to orders show facade

--- app/controllers/daily_menus_controller.rb
+++ app/controllers/daily_menus_controller.rb
@@ -2,13 +2,13 @@ class DailyMenusController < ApplicationController
   before_action :verify_is_not_admin!
   skip_before_action :authenticate_user!, except: [:show]
   before_action :find_daily_menu, only: [:show]
+  decorates_assigned :daily_menu
   
   def index
     @facade = DailyMenus::IndexFacade.new(params)
   end
 
   def show
-    @facade = DailyMenus::ShowFacade.new(@daily_menu, current_user)
   end
 
   private
---
+++ app/facades/orders/show_facade.rb
@@ -0,0 +1,17 @@
+module Orders
+  class ShowFacade
+    attr_reader :order
+    
+    def initialize(order)
+      @order = order
+    end
+    
+    def user_order_cost
+      @user_order_cost ||= order&.food_items&.pluck(:price)&.sum
+    end
+    
+    def user_order_items
+      @user_order_items ||= order&.food_items.decorate
+    end
+  end
+end
