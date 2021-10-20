1fdc87c104517c887e7a49356021efbf014330e9
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-10-20 12:32:22 +0300

    add delegete methods for order

--- app/decorators/order_decorator.rb
+++ app/decorators/order_decorator.rb
@@ -1,6 +1,8 @@
 class OrderDecorator < ApplicationDecorator
   decorates_association :daily_menu
-  
+  delegate :formatted_created_at, to: :daily_menu, prefix: true
+  delegate :name, to: :user, prefix: true
+
   def food_items_humanized
     food_items.map(&:name).to_sentence(last_word_connector: ', ')
   end
