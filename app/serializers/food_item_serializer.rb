b2cbbe0a593aa522ce74d1f3cb7010a1709290d2
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-10-28 16:35:25 +0300

    create serializers that needed to output todays orders list with details for each person

---
+++ app/serializers/food_item_serializer.rb
@@ -0,0 +1,8 @@
+class FoodItemSerializer < ActiveModel::Serializer
+  attributes :name, :course, :price
+  
+  def course
+    object.course.humanize
+  end
+end
+ 
---
+++ app/serializers/order_serializer.rb
@@ -0,0 +1,10 @@
+class OrderSerializer < ActiveModel::Serializer
+  has_many :food_items
+  belongs_to :user
+  attributes :id, :total_cost, :created_at
+
+  def total_cost
+    object.food_items&.pluck(:price)&.sum
+  end
+end
+ 
