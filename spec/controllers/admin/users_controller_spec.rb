55ae19f2f11c04700c75b38e8151109090abeed8
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-11-09 14:24:57 +0200

    refactor admin users controller

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -1,78 +1,75 @@
 require 'rails_helper'
 
 RSpec.describe Admin::UsersController, type: :controller do
-  describe 'unauthorized user' do
+  let!(:admin) { create(:user) }
+  let(:user)   { create(:random_user) }
+
+  context 'unauthenticated user' do
+    
     describe 'GET /index' do
-      it 'redirects to login page with alert flash' do
+      it 'redirects to login' do
         get :index
         expect(response).to redirect_to(new_user_session_url)
-        expect(flash[:alert]).to be_present
       end
     end
 
     describe 'GET /show' do
-      let(:user) { create(:user) }
-
-      it 'redirects to login page with alert flash' do
+      it 'redirects to login' do
         get :show, params: { id: user }
         expect(response).to redirect_to(new_user_session_url)
-        expect(flash[:alert]).to be_present
       end
     end
   end
 
-  describe 'authorized user' do
-    let(:user) { create(:user) }
-
-    before do
-      user.update(is_admin: false)
-      sign_in(user)
-    end
+  context 'authorized user' do
+    
+    before { sign_in(user) }
 
     describe 'GET /index' do
-      it 'redirects to root_path with alert flash' do
+      it 'redirects to root_path' do
         get :index
         expect(response).to redirect_to(root_path)
-        expect(flash[:alert]).to be_present
       end
     end
 
     describe 'GET /show' do
-      it 'redirects to admin root_path with alert flash' do
+      it 'redirects to root_path' do
         get :show, params: { id: user }
         expect(response).to redirect_to(root_path)
-        expect(flash[:alert]).to be_present
+      end
+    end
+  end
+
+  context 'authorized admin' do
+    
+    before { sign_in(admin) }
+
+    describe 'GET /index' do
+      it 'renders :index template' do
+        get :index
+        expect(response).to render_template(:index)
+      end
+
+      it "assigns @facade" do
+        get :index
+        expect(assigns(:facade)).to be_a(Admin::Users::IndexFacade)
       end
     end
 
-    describe 'authorized admin' do
-      let(:food_items_attributes) { attributes_for_list(:food_item, 3) }
-      let(:daily_menu) { create(:daily_menu, food_items_attributes: food_items_attributes) }
-      let(:admin) { create(:user) }
-      let(:user) { create(:random_user) }
-
-      before do
-        admin.update(is_admin: true)
-        sign_in(admin)
+    describe 'GET /show' do
+      it 'renders :show template' do
+        get :show, params: { id: user }
+        expect(response).to render_template(:show)
       end
 
-      describe 'GET /index' do
-        it 'renders index' do
-          get :index
-          expect(response).to render_template(:index)
-        end
+      it 'assigns @user' do
+        get :show, params: { id: user }
+        expect(assigns(:user)).to eq(user)
       end
 
-      describe 'GET /show' do
-        it 'renders show' do
-          get :show, params: { id: user }
-          expect(response).to render_template(:show)
-        end
-
-        it 'assigns requested user to @user' do
-          get :show, params: { id: user }
-          expect(assigns(:user)).to eq(user)
-        end
+      it 'assigns @facade' do
+        get :show, params: { id: user }
+        expect(assigns(:facade)).to be_a(Admin::Users::ShowFacade)
       end
     end
   end
