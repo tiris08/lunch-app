5a9a047a1b0abe17c582af2f3ec87acaac2a8ba0
Taras Ivanyuk <ivanyuk.taras@gmail.com>
2021-11-11 09:35:44 +0200

    refactor models specs

--- spec/models/daily_menu_spec.rb
+++ spec/models/daily_menu_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe DailyMenu, type: :model do
-  it { should accept_nested_attributes_for(:food_items).allow_destroy(true) }
-  it { should have_many(:food_items) }
-  it { should have_many(:orders) }
+  
+  describe "associations" do
+    it { should accept_nested_attributes_for(:food_items).allow_destroy(true) }
+    it { should have_many(:food_items) }
+    it { should have_many(:orders) }
+  end
+  
+  describe "validations" do
+    it { should validate_presence_of(:food_items)}
+  end
 end
