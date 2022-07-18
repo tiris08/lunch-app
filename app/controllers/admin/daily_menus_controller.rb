6f60427ee333eb0754efd4e8020d19347ad7cba6
Taras <ivanyuk.taras@gmail.com>
2022-07-18 09:41:49 +0300

    Add image upload functionality with active_storage

--- app/controllers/admin/daily_menus_controller.rb
+++ app/controllers/admin/daily_menus_controller.rb
@@ -54,7 +54,7 @@ class Admin::DailyMenusController < Admin::BaseController
   private
 
   def daily_menu_params
-    params.require(:daily_menu).permit(food_items_attributes: %i[id name price course _destroy])
+    params.require(:daily_menu).permit(food_items_attributes: %i[id name price course image _destroy])
   end
 
   def find_daily_menu
--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -1,6 +1,7 @@
 class FoodItem < ApplicationRecord
   enum course: { first_course: 0, main_course: 1, drink: 2 }
   belongs_to :daily_menu
+  has_one_attached :image
   has_many :order_items, dependent: :destroy
   has_many :orders, through: :order_items
   validates :name, presence: true
