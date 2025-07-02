82119988a82e11d5ff99a3d0eae50462247ab70f
Taras <ivanyuk.taras@gmail.com>
2025-07-02 18:40:16 -0500

    Vcr record request specs

--- spec/support/helpers/request_token.rb
+++ spec/support/helpers/request_token.rb
@@ -4,21 +4,23 @@ module Helpers
     require 'net/http'
 
     def request_token
-      url = URI("https://#{Rails.application.credentials.auth0[:domain]}/oauth/token")
+      VCR.use_cassette('auth0_token') do
+        url = URI("https://#{Rails.application.credentials.auth0[:domain]}/oauth/token")
 
-      http = Net::HTTP.new(url.host, url.port)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        http = Net::HTTP.new(url.host, url.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-      request = Net::HTTP::Post.new(url)
-      request['content-type'] = 'application/json'
-      request.body = %({\"client_id\":\"#{Rails.application.credentials.auth0[:client_id]}\",
-                      \"client_secret\":\"#{Rails.application.credentials.auth0[:client_secret]}\",
-                      \"audience\":\"#{Rails.application.credentials.auth0[:api_identifier]}\",
-                      \"grant_type\":\"client_credentials\"})
+        request = Net::HTTP::Post.new(url)
+        request['content-type'] = 'application/json'
+        request.body = %({\"client_id\":\"#{Rails.application.credentials.auth0[:client_id]}\",
+                        \"client_secret\":\"#{Rails.application.credentials.auth0[:client_secret]}\",
+                        \"audience\":\"#{Rails.application.credentials.auth0[:api_identifier]}\",
+                        \"grant_type\":\"client_credentials\"})
 
-      response = http.request(request)
-      JSON.parse(response.body)['access_token']
+        response = http.request(request)
+        JSON.parse(response.body)['access_token']
+      end
     end
   end
 end
